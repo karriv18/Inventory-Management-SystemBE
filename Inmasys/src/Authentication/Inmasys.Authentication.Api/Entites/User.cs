8ac0b7fc8734f97d4d8aeb959a87ff79aa87c9b7
karriv18 <krlclarence@gmail.com>
2025-12-02 22:33:30 +0800

    feat authentication not yet done

---
+++ Inmasys/src/Authentication/Inmasys.Authentication.Api/Entites/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Inmasys.Authentication.Api.Entites;
+
+[Table("users")]
+public class User
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("user_name")]
+    public string UserName { get; set; } = string.Empty;
+
+    [Column("password_hash")]
+    public string? PasswordHash {  get; set; }
+}
